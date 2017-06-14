b56fb45eff09e93f14d0a766f50f4df82b4471c0
cyang <cyang112@asu.edu>
2017-06-14 13:00:16 -0700

    This is the final version for project4

---
+++ Week 04/CSE360/Team7Proj2Panel.java
@@ -0,0 +1,118 @@
+package CSE360;
+
+import java.awt.*;
+import javax.swing.*;
+import java.awt.event.*;
+
+public class Team7Proj2Panel extends JPanel{
+    String result;
+    Object[] cityList = {"Tempe", "New York", "LA", "Chicago", "Houston", "Dallas", "Austin", "Detroit", "Boston", "Seattle"};
+    double latitude = 33.4255, longitude = -111.9400;
+    Team7WeatherPanel weather;
+    Team7googleMap map;
+    
+    Team7Proj2Panel()
+    {
+        setLayout(new java.awt.GridBagLayout());
+        GridBagConstraints c = new GridBagConstraints();
+        c.fill = GridBagConstraints.BOTH;
+        c.gridx=0;c.gridy=0;
+        c.gridheight=1;
+        c.gridwidth=1;
+        c.weightx = 0.6666;
+        c.weighty = 1;
+        setVisible(true);
+        
+//        JButton choosingCity = new JButton("Choose City");
+//        choosingCity.addActionListener(new cityChoosingListener());
+//        add(choosingCity);
+        
+        
+        map = new Team7googleMap(latitude, longitude);
+        add(map);
+        c.gridx=1;c.weightx = 0.333;
+        weather = new Team7WeatherPanel(latitude, longitude);
+        add(weather);       
+    }
+    public void DisplayGeoMenu() {
+            result = (String)JOptionPane.showInputDialog(
+                    null,
+                    "Select a city:",
+                    "",
+                    JOptionPane.PLAIN_MESSAGE,
+                    null,
+                    cityList,
+                    "Tempe");
+            
+            if(result.equals("Tempe"))
+            {
+                latitude = 33.4255;
+                longitude = -111.9400;
+            }
+            else if(result.equals("New York"))
+            {
+                latitude = 40.7128;
+                longitude = -74.0059;
+            }
+            else if(result.equals("LA"))
+            {
+                latitude = 34.0522;
+                longitude = -118.2437;
+            }
+            else if(result.equals("Chicago"))
+            {
+                latitude = 41.8781;
+                longitude = -87.6298;
+            }
+            else if(result.equals("Houston"))
+            {
+                latitude = 29.7604;
+                longitude = -95.3698;
+            }
+            else if(result.equals("Dallas"))
+            {
+                latitude = 32.7767;
+                longitude = -96.7970;
+            }
+            else if(result.equals("Austin"))
+            {
+                latitude = 30.2672;
+                longitude = -97.7431;
+            }
+            else if(result.equals("Detroit"))
+            {
+                latitude = 42.3314;
+                longitude = -83.0458;
+            }
+            else if(result.equals("Boston"))
+            {
+                latitude = 42.3601;
+                longitude = -71.0589;
+            }
+            else
+            {
+                latitude = 47.6062;
+                longitude = -122.3321;
+            }
+            
+            
+            map.updateMap(latitude, longitude);
+            weather.updateWeatherPanel(latitude, longitude);
+            //remove(weather);
+            //weather = new Team7WeatherPanel(latitude, longitude);
+            //add(weather);
+            
+            revalidate();
+            repaint();
+        }
+    private class cityChoosingListener implements ActionListener
+    {
+        public void actionPerformed(ActionEvent e)
+        {
+            DisplayGeoMenu();
+        }
+    }
+
+
+    
+}
---
+++ Week 04/CSE360/Team7googleMap.java
@@ -0,0 +1,67 @@
+package CSE360;
+
+import java.awt.*;
+import javax.swing.*;
+
+import java.io.FileReader;
+import java.lang.String;
+import java.io.*;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.Reader;
+import java.net.URL;
+import java.nio.charset.Charset;
+
+class Team7googleMap extends JPanel{
+    double latitude,longitude;
+    private JLabel map;
+    //constructor
+    public Team7googleMap(double latitude, double longitude)     {
+    	updateGeoLoc(latitude,longitude);
+        //add(new JLabel(new ImageIcon((new ImageIcon("mycity.jpg")).getImage().getScaledInstance(250, 250,
+                //java.awt.Image.SCALE_SMOOTH))));
+        //setVisible(true);
+        map =new JLabel(new ImageIcon((new ImageIcon("mycity.jpg")).getImage().getScaledInstance(200, 200,
+                java.awt.Image.SCALE_SMOOTH))); 
+        add(map);
+        setVisible(true);
+        setOpaque(false);
+    }
+    public void updateMap(double latitude, double longitude) { 
+        updateGeoLoc(latitude,longitude);
+        map.setIcon(new ImageIcon((new ImageIcon("mycity.jpg")).getImage().getScaledInstance(200, 200,
+                java.awt.Image.SCALE_SMOOTH)));
+        this.revalidate(); this.repaint();
+    }
+    private void updateGeoLoc(double latitude, double longitude)     {
+    	this.latitude = latitude;
+    	this.longitude = longitude;
+    	
+   
+    	int zoom = 11;
+    	
+        try {
+            String imageUrl = "https://maps.googleapis.com/maps/api/staticmap?" + "center=" + latitude + "," + longitude + 
+            		"&zoom=" + zoom + "&size=640x640&maptype=road";
+            String outputImageFile = "mycity.jpg";
+            URL url = new URL(imageUrl);
+            InputStream is = url.openStream();
+            OutputStream os = new FileOutputStream(outputImageFile);
+
+            byte[] b = new byte[2048];
+            int length;
+
+            while ((length = is.read(b)) != -1) {
+                os.write(b, 0, length);
+            }
+
+            is.close();
+            os.close();
+        } catch (IOException e) {
+            System.exit(1);
+        }
+    }
+}
