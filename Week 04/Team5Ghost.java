fd6ee08c1657aa1532d17764a11c4a408f89847a
CSE360-Team5 <melissa.day@asu.edu>
2017-06-13 22:13:23 -0700

    Ghost position updated

--- Week 04/Team5Ghost.java
+++ Week 04/Team5Ghost.java
@@ -12,10 +12,10 @@ public class Team5Ghost extends JPanel implements Runnable {
 	
 	public Thread movingGhost;
 	JPanel ghostPanel = new JPanel();
-	int dataPanelWidth = 300;
-	int dataPanelHeight = 300;
+	int dataPanelWidth = 50;
+	int dataPanelHeight = 50;
 	int x = 5;
-	int y = 5;
+	int y = 3;
 	
 	//ImageIcon ghostImage = new ImageIcon("src/CSE360/Team5Images/Ghost.png");
 	//ImageIcon ghostImage = new ImageIcon("C:/Users/Melissa/Documents/Eclipse/CSE360/src/Team5Images/Ghost.png");
@@ -54,19 +54,36 @@ public class Team5Ghost extends JPanel implements Runnable {
      * Updates ghost's position
      */
  	public void run() {
+ 		int xChange = 5;
+ 		int yChange = 3;
+ 		int xvalueChange = xChange;
+ 		int yvalueChange = yChange;
+ 		
  		ghostPanel.revalidate();
  		ghostPanel.repaint();
  		try {
  			for (int i = 1; i < 100000; i++) {
  				
- 				x = x + 5;	// updates ghost's x position
- 				y = y + 5; // updates ghost's y position
+ 				x = x + xvalueChange;	// updates ghost's x position
+ 				y = y + yvalueChange; // updates ghost's y position
  				
  				if (x > dataPanelWidth) { // update ghost's location so it stays on panel
- 					x = 5;
+ 					
+ 					xvalueChange = -xChange;
+ 					//x = 5;
  				}
  				if (y > dataPanelHeight) { // update ghost's location so it stays on panel
- 					y = 5;
+ 					
+ 					yvalueChange = -yChange;
+ 					//y = 5;
+ 				}
+ 				
+ 				if (x < 0) {
+ 					xvalueChange = xChange;
+ 				}
+ 				
+ 				if (y < 0) {
+ 					yvalueChange = yChange;
  				}
  				ghostPanel.setBounds(x, y, 100, 100);
  				
