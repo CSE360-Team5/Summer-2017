fa7fe9087fa76efec10ecdd0221f2579116f495f
CSE360-Team5 <melissa.day@asu.edu>
2017-06-14 12:00:30 -0700

    Cover updated with one panel

--- Week 04/Team5Cover.java
+++ Week 04/Team5Cover.java
@@ -7,63 +7,67 @@ import java.awt.event.ActionListener;
 
 import javax.swing.*;
 
-
 public class Team5Cover extends JPanel implements ActionListener {
 	JButton button;
+	JPanel main;
 
 	public Team5Cover() {
+		main = new JPanel();
+		// Add border
+		main.setBorder(BorderFactory.createLineBorder(Color.green, 5));
 
-	  // Add border
-	  setBorder(BorderFactory.createLineBorder(Color.green, 5));
-	  
-	  // Add names
-      JLabel label1 = new JLabel("Team 5:");
-      JLabel label2 = new JLabel("Melissa Day,");
-      JLabel label3 = new JLabel("Austin McCleary,");
-      JLabel label4 = new JLabel("Yuxue Zhou,");
-      JLabel label5 = new JLabel("Zelin Bao");
-    
-      // set font & size
-      label1.setFont(new Font("Rockwell", 1, 20));
-      
-    
-      
-      add(label1);
-      add(label2);
-      add(label3);
-      add(label4);
-      add(label5);
-     
-     
-      
-      button = new JButton(new ImageIcon("/Users/zelinbao/Desktop/setting.jpg"));
-      button.setPreferredSize(new Dimension(90,50));
-      button.addActionListener(this);
-      button.setVisible(true);
-      button.setBounds(150, 70, 100, 100);
-      
-      this.setLayout(new FlowLayout());
-      this.setBounds(125, 80, 500, 500);
-      this.setVisible(true);
-      //this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);  
-     
-      
-    
-    add(button); 
-  }
+		// Add names
+		JLabel label1 = new JLabel("Team 5:");
+		JLabel label2 = new JLabel("Melissa Day,");
+		JLabel label3 = new JLabel("Austin McCleary,");
+		JLabel label4 = new JLabel("Yuxue Zhou,");
+		JLabel label5 = new JLabel("Zelin Bao");
 
-	
+		// set font & size
+		label1.setFont(new Font("Rockwell", 1, 20));
+
+		main.add(label1);
+		main.add(label2);
+		main.add(label3);
+		main.add(label4);
+		main.add(label5);
+
+		button = new JButton(new ImageIcon("/Users/zelinbao/Desktop/setting.jpg"));
+		button.setPreferredSize(new Dimension(90, 50));
+		button.addActionListener(this);
+		button.setVisible(true);
+		//button.setBounds(150, 70, 100, 100);
+
+		//main.setLayout(new FlowLayout());
+		//main.setBounds(125, 80, 500, 500);
+		//main.setVisible(true);
+		// this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+		main.add(button);
+		
+		add(main);
+	}
+
 	public void actionPerformed(ActionEvent e) {
 
 		if (e.getSource() == button) {
-			Team5City T5 = new Team5City(); 
 			
-			JFrame main = new JFrame();
-			main.add(T5);
-			main.setVisible(true);
-			main.setBounds(300, 300,600, 500);
+			
+			Team5City T5 = new Team5City();
 
-		}   
-	}  
+			main.removeAll();
+			main.setBorder(BorderFactory.createEmptyBorder());
+			
+			
+			// JFrame main = new JFrame();
+			main.add(T5);
+			
+			revalidate();
+			repaint();
+			//main.setVisible(true);
+			//main.setBounds(300, 300, 600, 500);
+			
+		}
+	}
 
 }
