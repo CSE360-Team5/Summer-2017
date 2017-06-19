a027b6c70e8cbf81a160a5ed3bcbb37897288d73
CSE360-Team5 <melissa.day@asu.edu>
2017-06-19 14:43:38 -0700

    Update Team5Layers.java

--- Week 04/Team5Layers.java
+++ Week 04/Team5Layers.java
@@ -1,5 +1,13 @@
 package CSE360;
 
+/*
+ * Team 5
+ * @author Melissa Day
+ * @author Austin McCleary
+ * @author Zelin Bao
+ * @author Yuxue Zhou
+ */
+
 import java.awt.BorderLayout;
 import java.awt.Dimension;
 import java.awt.Insets;
@@ -13,23 +21,35 @@ public class Team5Layers extends JPanel {
 	
 	public Team5Layers() {
 		JPanel complete = new JPanel();
-		JPanel ghost = new JPanel();
-		ghost.add(new Team5Ghost());
+		//JPanel ghost = new JPanel();
+		//ghost.add(new Team5Ghost());
 		JPanel weather = new JPanel();
 		weather.add(new Team5City());
+		
 		// create layered pane
     	JLayeredPane layers = new JLayeredPane();
     	layers.setLayout(new BorderLayout());
-    	layers.add(ghost, 1);
+    	//layers.add(ghost, 1);
     	layers.add(weather, 0);
+    	//ghost.setOpaque(false);
+    	
     	Insets insets = layers.getInsets();
-    	Dimension size = ghost.getPreferredSize();
-    	ghost.setBounds(0 + insets.left, 0 + insets.top,
-    	             size.width, size.height);
+    	//Dimension size = ghost.getPreferredSize();
+    	weather.setSize(250, 155);
+    	Dimension size = weather.getPreferredSize();
+    	
+    	//ghost.setBounds(0 + insets.left, 0 + insets.top,
+    	            // size.width, size.height);
     	size = weather.getPreferredSize();
     	weather.setBounds(0 + insets.left, 0 + insets.top,
     	             size.width, size.height);
-		complete.add(layers);	
+		
+    	layers.revalidate();
+    	layers.repaint();
+    	complete.add(layers);
+    	
+    
+		
 		add(complete);
 	}
 }
